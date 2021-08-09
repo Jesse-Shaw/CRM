3fcf5e888d392035146d2a5e09c23a2b586b9b58
xjx <jxiaoao@connect.ust.hk>
2021-08-09 20:26:58 +0800

    2021.08.09

--- crm/src/main/java/com/bjpowernode/crm/workbench/web/controller/TranController.java
+++ crm/src/main/java/com/bjpowernode/crm/workbench/web/controller/TranController.java
@@ -9,11 +9,14 @@ import com.bjpowernode.crm.utils.ServiceFactory;
 import com.bjpowernode.crm.utils.UUIDUtil;
 import com.bjpowernode.crm.workbench.domain.Activity;
 import com.bjpowernode.crm.workbench.domain.Clue;
+import com.bjpowernode.crm.workbench.domain.Customer;
 import com.bjpowernode.crm.workbench.domain.Tran;
 import com.bjpowernode.crm.workbench.service.ActivityService;
 import com.bjpowernode.crm.workbench.service.ClueService;
+import com.bjpowernode.crm.workbench.service.CustomerService;
 import com.bjpowernode.crm.workbench.service.impl.ActivityServiceImpl;
 import com.bjpowernode.crm.workbench.service.impl.ClueServiceImpl;
+import com.bjpowernode.crm.workbench.service.impl.CustomerServiceImpl;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -31,10 +34,18 @@ public class TranController extends HttpServlet {
         String path = request.getServletPath();
         if ("/workbench/transaction/add.do".equals(path)) {
             add(request,response);
-        } else if ("/workbench/transaction/.do".equals(path)) {
-            //xxx(request,response);
+        } else if ("/workbench/transaction/getCustomerName.do".equals(path)) {
+            getCustomerName(request,response);
         }
 
+    }
+
+    private void getCustomerName(HttpServletRequest request, HttpServletResponse response) {
+        System.out.println("取得客户列表按照客户名称进行模糊查询");
+        String name = request.getParameter("name");
+        CustomerService customerService = (CustomerService) ServiceFactory.getService(new CustomerServiceImpl());
+        List<String> stringList = customerService.getCustomerName(name);
+        PrintJson.printJsonObj(response,stringList);
     }
 
     private void add(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
